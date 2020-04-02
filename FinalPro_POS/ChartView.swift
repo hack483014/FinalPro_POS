24237f3421816352ec1ced0c8ba8c8e162736a93
POS <tharathorn.laongtiparos@g.swu.ac.th>
2020-04-02 21:43:39 +0700

    Initial Commit

---
+++ FinalPro_POS/ChartView.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  FinalPro_POS
+//
+//  Created by Dream on 19/3/2563 BE.
+//  Copyright © 2563 bank. All rights reserved.
+//
+
+import UIKit
+//import Charts
+//
+//class ViewController: UIViewController {
+//    
+//   
+//    @IBOutlet weak var lineChartView: LineChartView!
+//    
+////    @IBAction func random(_ sender: UIButton) {
+////        let count = Int(arc4random_uniform(20) + 3)
+////        setChartValues(count)
+////    }
+//    
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setChartValues()
+//        
+//    }
+//    
+//    func setChartValues(){
+//        let values = (0..<3).map {(i) -> ChartDataEntry in
+//            let val = Double(UInt32())
+//            return ChartDataEntry(x: Double(i), y:val)
+//        }
+//        let set1 = LineChartDataSet(entries: values, label: "ยอดขาย(รายวัน)")
+//        let data = LineChartData(dataSet: set1)
+//        
+//        self.lineChartView.data = data
+//    }
+//
+//}
+//
